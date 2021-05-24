9f7dcc0db77e4e5cdeaeec6503aba51be8a1ea89
chavegas <43023956+chavegas@users.noreply.github.com>
2021-05-24 16:41:30 +1000

    updated

--- as2submission.py
+++ as2submission.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession, Row
-from pyspark.ml.feature import PCA
 from pyspark.ml.clustering import KMeans
-from pyspark.ml.feature import HashingTF, IDF, Tokenizer, Normalizer, VectorAssembler, StringIndexer, IndexToString
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, Normalizer, VectorAssembler, StringIndexer, IndexToString, CountVectorizer
 from pyspark.ml.linalg import *
 from pyspark.sql.types import * 
 from pyspark.sql.functions import *
@@ -15,26 +14,26 @@ def cos_sim(a,b):
 def blank_as_null(x):
         return when(col(x) != "", col(x)).otherwise(None)
 
-if __name__ == "__main__":
-    spark = SparkSession \
-        .builder \
-        .appName("Assignment 2 WL 1") \
-        .getOrCreate()
-    spark.conf.set("spark.sql.shuffle.partitions",100)    
-    tweeets_data = spark.read.option('multiline','true').json('tweets.json')    
-    tweets_agg = tweeets_data.groupby("user_id").agg(f.concat_ws(" ", f.collect_list(tweeets_data.retweet_id)).alias('agg_retweets'),
-                                        f.concat_ws(" ", f.collect_list(tweeets_data.replyto_id)).alias('agg_replies')).cache()
+def process_data_wl1(data):
+    tweets_agg = data.groupby("user_id").agg(f.concat_ws(" ", f.collect_list(data.retweet_id)).alias('agg_retweets'),
+                                        f.concat_ws(" ", f.collect_list(data.replyto_id)).alias('agg_replies')).cache()
     tweets_agg = tweets_agg.withColumn("agg_retweets", blank_as_null("agg_retweets"))\
         .withColumn("agg_replies", blank_as_null("agg_replies"))  
-                                    
-
     tweets_processed = tweets_agg.select('*',concat_ws(' ','agg_retweets','agg_replies').alias('agg_tweet_respond'))
 
     tokenizer = Tokenizer(inputCol='agg_tweet_respond',
         outputCol="vectors")
-    tweets_vectors = tokenizer.transform(tweets_processed).cache()
-    hashingTF = HashingTF(inputCol="vectors", outputCol="tf")
-    tf = hashingTF.transform(tweets_vectors)
+    tweets_vectors = tokenizer.transform(tweets_processed)
+    return tweets_vectors
+
+def apply_tf1(data,hashtf):
+    if hashtf == True:
+        hashingTF = HashingTF(inputCol="vectors", outputCol="tf")
+        tf = hashingTF.transform(data)
+    else:
+        cv = CountVectorizer(inputCol="vectors", outputCol="tf")
+        tweets_cv = cv.fit(data)
+        tf = tweets_cv.transform(data)
 
     selected_id = 202170318
     tweets_user_filtered = tf.where(f'user_id = {selected_id}')
@@ -45,40 +44,25 @@ if __name__ == "__main__":
     tf = tf.withColumn("CosineSim",cos_function('tf'))
     tf = tf.where(f'user_id <> {selected_id}')
     sorted_output_tf = tf.filter(tf.CosineSim > 0).sort(col('CosineSim').desc())
-    sorted_output_tf.select(col('user_id').alias(f'Top Most Similar User IDs to {selected_id}'),
-        col('CosineSim').alias(f'CosineSim using TFIDF Method')).show(5,truncate=False)
+    return sorted_output_tf.select(col('user_id').alias(f'Top Most Similar User IDs to {selected_id}'),
+        col('CosineSim').alias(f'CosineSim')).show(5,truncate=False)
 
-    from pyspark.ml.feature import CountVectorizer
-
-    cv = CountVectorizer(inputCol="vectors", outputCol="features")
-
-    tweets_cv = cv.fit(tweets_vectors)
-
-    cv_data = tweets_cv.transform(tweets_vectors)
-    tweets_user_filtered = cv_data.where(f'user_id = {selected_id}')
-    compare_vector = tweets_user_filtered.first()['features']
-    cos_function = udf(lambda x: cos_sim(x,compare_vector), FloatType())
-
-    cv_data = cv_data.withColumn("CosineSim",cos_function('features'))
-    cv_data = cv_data.where(f'user_id <> {selected_id}')
-    sorted_output_cv = cv_data.filter(cv_data.CosineSim > 0).sort(col('CosineSim').desc())
-    sorted_output_cv.select(col('user_id').alias(f'Top Most Similar User IDs to {selected_id}'),
-        col('CosineSim').alias(f'CosineSim using CountVectorizer Method')).show(5,truncate=False)
-
-    wl2 = tweeets_data.withColumn("mentioned_users", tweeets_data["user_mentions"].getField('id')).cache()
+def process_data_wl2(data):
+    wl2 = data.withColumn("mentioned_users", data["user_mentions"].getField('id')).cache()
     wl2_users = wl2.select(col('user_id'),col('mentioned_users'))
     wl2_users = wl2_users.withColumn("mentioned_users", explode("mentioned_users"))
     wl2_users_agg = wl2_users.groupBy(col('user_id'),col('mentioned_users')).count().cache()
+    return wl2_users_agg
 
+def apply_wl2(data):
     stringIndexer_uid = StringIndexer(inputCol="user_id", outputCol="user_id_indexed",stringOrderType="frequencyDesc")
-    model_uid = stringIndexer_uid.fit(wl2_users_agg)
+    model_uid = stringIndexer_uid.fit(data)
 
     stringIndexer_mentioned = StringIndexer(inputCol="mentioned_users", outputCol="mentioned_users_indexed",stringOrderType="frequencyDesc")
-    model_mu = stringIndexer_mentioned.fit(wl2_users_agg)
+    model_mu = stringIndexer_mentioned.fit(data)
 
-    td = model_uid.transform(wl2_users_agg)
+    td = model_uid.transform(data)
     wl2_users_transformed = model_mu.transform(td)
-
     als = ALS(userCol="user_id_indexed", itemCol="mentioned_users_indexed", ratingCol="count",
             coldStartStrategy="drop")
     model = als.fit(wl2_users_transformed)
@@ -98,4 +82,21 @@ if __name__ == "__main__":
 
     model_recs = model_recs.withColumn("recommendations", recommendations)\
             .withColumn("user_id", uid_labels_[col("user_id_indexed")])
-    model_recs.select(['user_id'] + [col("recommendations")[i].alias('Recommended User: '+str(i+1)) for i in range(n)]).show(truncate=False)            
+    return model_recs.select(['user_id'] + [col("recommendations")[i].alias('Recommended User: '+str(i+1)) for i in range(n)]).show(truncate=False)            
+
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("Assignment 2 WL 1") \
+        .getOrCreate()
+    spark.conf.set("spark.sql.shuffle.partitions",100)    
+    tweeets_data = spark.read.option('multiline','true').json('tweets.json')    
+    
+    tweets_processed = process_data_wl1(tweeets_data).cache()
+
+    apply_tf1(tweets_processed,True)
+    apply_tf1(tweets_processed,False)
+
+    tweets_processedwl2 = process_data_wl2(tweeets_data).cache()
+    apply_wl2(tweets_processedwl2)    
